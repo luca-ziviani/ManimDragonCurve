b976297b1cc30574a25dc249c83a9d5660c446b2
luca-ziviani <lucaziv98@gmail.com>
2025-06-09 17:16:42 +0200

    Comment DragonCurve.py

--- DragonCurve.py
+++ DragonCurve.py
@@ -2,25 +2,28 @@ from manim import *
 
 class DragonCurve(Scene):
     def construct(self):
-        n = 10
-        string = 'F'
-        step = 8. * 2**(-n/2)
+        n = 10                         # Iteration counter
+        string = 'F'                   # Initial string
+        step = 8. * 2**(-n/2)          # Length of one step
+        point = np.array([0,0,0])      # Starting point 
 
-        point = np.array([0,0,0])
+        # Initial orientation
         direction = np.array([-np.cos(np.pi*n/4), np.sin(np.pi*n/4),0])
 
+        # Matrix of the rotations (90°)
         turn_left = np.array([[0.,-1.,0.],[1.,0.,0.],[0.,0.,0.]])
         turn_right = np.array([[0.,1.,0.],[-1.,0.,0.],[0.,0.,0.]])
 
+        # Initialization
         path = VMobject()
         points = [point]
         
-        # Replace F by F+G
-        # replace G by F-G
+        # Replace F by F+G , replace G by F-G
         replacements = str.maketrans({"F": "F+G", "G": "F-G"})
         for i in range(0, n):
             string = string.translate(replacements)
-        
+
+        # Follow the string instructions
         for c in string:
             if c=="F" or c == "G":
                 # Move forward
@@ -35,13 +38,16 @@ class DragonCurve(Scene):
                 # Turn Right
                 direction = turn_right.dot(direction)
 
+        # Define path
         path.set_points_as_corners(points)
         path.center()
-        
+
+        # Play: Title
         text = Text("Dragon Curve", color=RED, font_size = 60)
         self.play(FadeIn(text), run_time=1)
         self.wait(1)
         self.play(FadeOut(text),run_time=1)
 
+        # Play: Curve
         self.play(Create(path), run_time=2**(n/2), rate_function=linear)
         self.wait(2)
